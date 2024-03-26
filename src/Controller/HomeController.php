978200b4e92f8cfc97ab9fa0eced2593d9421003
iamam4 <alexandre.moreau@edu.univ-eiffel.fr>
2024-03-26 17:48:57 +0100

    Symfony

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/home', name: 'app_home')]
+    public function index(): Response
+    {
+
+        if ($this->getUser()) {
+            // if logged in, $usernames will be the username
+            $usernames = $this->getUser()->getUsername();
+        } else {
+            // if not logged in, $usernames will be null
+            $usernames = null;
+        }
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'usernames' => $usernames,
+        ]);
+    }
+}
